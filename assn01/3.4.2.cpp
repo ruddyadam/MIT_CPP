74833cdf99c66a1189a2b4ae328b2fd180a1a8c1
Adam Ekelund <ruddyadam@gmail.com>
2017-09-15 06:20:14 -0500

    minor change to assn01/3.4.2.cpp
    Signed-off-by: Adam Ekelund <ruddyadam@gmail.com>

--- assn01/3.4.2.cpp
+++ assn01/3.4.2.cpp
@@ -8,7 +8,7 @@ int main(){
 	while(1){
 	cout << "Enter a positive or negative number" << endl;
 	cin >> number;
-	if (number%5!=0 or number<0) continue;
+	if (number%5!=0 and number<0) continue;
 		cout << number/5 << endl;
 	}
 }
