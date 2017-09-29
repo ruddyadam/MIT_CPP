beec123a6b746acaf3786e3a3ec2a6fc2de200fc
Adam Ekelund <ruddyadam@gmail.com>
2017-09-29 05:46:08 -0500

    more edits to assn02_4.5.cpp
    Signed-off-by: Adam Ekelund <ruddyadam@gmail.com>

--- assn02/assn02_4.5.cpp
+++ assn02/assn02_4.5.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
-int size;
-std::cout << "How many numbers will you be adding? " + std::endl;
-std::cin >> size;
-int arrayOfNums[size];
-
-int sumNumbers(int size, int arrayOfNums){
+int sumNumbers(int size, int arrayOfNums[]){
     int sums = 0;
     for(int i=0;i<size;i++){
         arrayOfNums[i] += sums;
@@ -13,11 +8,15 @@ int sumNumbers(int size, int arrayOfNums){
     return sums;
 }
 int main(){
-    std::cout << "Please enter numbers: " + std::endl;
+    int size;
+    std::cout << "How many numbers will you be adding? " << std::endl;
+    std::cin >> size;
+    int arrayOfNums[size];
+    std::cout << "Please enter numbers: " << std::endl;
     int numForArray = 0;
-    for(int i=0,i<size,i++){
+    for(int i=0;i<size;i++){
         std::cin >> numForArray;
         arrayOfNums[i] = numForArray;
     }
-    std::cout << addNumbers(size, arrayOfNums);
+    std::cout << sumNumbers(size, arrayOfNums);
 }
