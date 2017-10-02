2f63df89bcf9c6825867918c8394e53254607008
Adam Ekelund <ruddyadam@gmail.com>
2017-10-01 20:26:44 -0500

    edits to assn02_5.3.cpp

---
+++ assn02/assn02_5.3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+
+//This program is a Monte Carlo technique for calculating Pi
+
+using namespace std;
+
+void checkForHit(double xCoord, double yCoord, int *hits){
+    if (sqrt(pow(xCoord,2.0) + pow(yCoord,2.0)) <= 1){
+        *hits += 1;
+    }
+}
+
+main(){
+    int n=5000000;   //throws
+    int hits;        //landed in circle
+    float pi;
+    srand(time(NULL));
+
+    for(int i=0; i<n;i++){
+        double xCoord = rand() / double (RAND_MAX);
+        double yCoord = rand() / double (RAND_MAX);
+        checkForHit(xCoord, yCoord, &hits);
+        //cout << i;
+    }
+    cout << "Hits: " << hits << endl;
+    cout << "Total Thrown: " << n << endl;
+    
+    pi = 4*((float)(hits)/(float)(n));
+    cout << "Monte Carlo estimate of Pi: " << pi;
+    //cout << pi;
+    return 0;
+}
